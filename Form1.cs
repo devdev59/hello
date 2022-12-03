9de626c9aa100c522bde47cf06554dd511ee5acd
devdev59 <119765037+devdev59@users.noreply.github.com>
2022-12-04 01:05:59 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        int globaldeger = 20;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = globaldeger.ToString();
+            globaldeger = 555;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Text = globaldeger.ToString();
+            globaldeger = 888;
+
+        }
+    }
+}
